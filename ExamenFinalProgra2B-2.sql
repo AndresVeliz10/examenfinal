3a5e6e9dae6b00360115091a47f82c1a35ce9595
AndresVeliz10 <93888913+AndresVeliz10@users.noreply.github.com>
2021-11-07 18:11:09 -0600

    Add files via upload

--- ExamenFinalProgra2B-2.sql
+++ ExamenFinalProgra2B-2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 07-11-2021 a las 22:49:12
+-- Tiempo de generación: 08-11-2021 a las 01:08:41
 -- Versión del servidor: 10.4.21-MariaDB
 -- Versión de PHP: 8.0.11
 
@@ -18,30 +18,8 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `jugadores`
+-- Base de datos: `examenfinalprogra2b-2`
 --
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `jugadores`
---
-
-CREATE TABLE `jugadores` (
-  `codigo` int(11) NOT NULL,
-  `nombre` varchar(45) DEFAULT NULL,
-  `numerocamisola` int(11) DEFAULT NULL,
-  `posicion` varchar(45) DEFAULT NULL,
-  `foto` varchar(45) DEFAULT NULL,
-  `edad` int(11) DEFAULT NULL,
-  `habilidades` varchar(45) DEFAULT NULL,
-  `piernahabil` varchar(45) DEFAULT NULL,
-  `nacionalizado` varchar(45) DEFAULT NULL,
-  `codigoequipo` int(11) DEFAULT NULL,
-  `codigodivision` int(11) DEFAULT NULL,
-  `codigomarca` int(11) DEFAULT NULL,
-  `valormercado` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -59,7 +37,19 @@ CREATE TABLE `marcas` (
 --
 
 INSERT INTO `marcas` (`codigo`, `nombre`) VALUES
-(7, 'Juan Jose Salazar');
+(7, 'Juan Jose Salazar'),
+(9, 'MARTINO GARZA');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `modelo`
+--
+
+CREATE TABLE `modelo` (
+  `codigo` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -85,15 +75,15 @@ INSERT INTO `paises` (`codigo`, `nombre`, `imagenbandera`) VALUES
 --
 
 --
--- Indices de la tabla `jugadores`
---
-ALTER TABLE `jugadores`
-  ADD PRIMARY KEY (`codigo`);
-
---
 -- Indices de la tabla `marcas`
 --
 ALTER TABLE `marcas`
+  ADD PRIMARY KEY (`codigo`);
+
+--
+-- Indices de la tabla `modelo`
+--
+ALTER TABLE `modelo`
   ADD PRIMARY KEY (`codigo`);
 
 --
@@ -107,16 +97,16 @@ ALTER TABLE `paises`
 --
 
 --
--- AUTO_INCREMENT de la tabla `jugadores`
---
-ALTER TABLE `jugadores`
-  MODIFY `codigo` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT de la tabla `marcas`
 --
 ALTER TABLE `marcas`
-  MODIFY `codigo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `codigo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT de la tabla `modelo`
+--
+ALTER TABLE `modelo`
+  MODIFY `codigo` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `paises`
